2e49ef422f9adc48b4f08a8a387391a4131f1dfe
erensarac <esarac@outlook.com.tr>
2024-08-18 06:28:33 +0300

    feat: add color variables to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: [],
   theme: {
+    colors: {
+      "dark-blue": "hsl(209, 23%, 22%)",
+      "very-dark-blue": "hsl(207, 26%, 17%)",
+      "very-dark-blue-alt": "hsl(200, 15%, 8%)",
+      "dark-gray": "hsl(0, 0%, 52%)",
+      "very-light-gray": "hsl(0, 0%, 98%)",
+      "white": "hsl(0, 0%, 100%)"
+    },
     extend: {},
   },
   plugins: [],
